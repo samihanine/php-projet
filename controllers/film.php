0f6479c3eeacfc22706958a45f086f40fb2cd8aa
sam <sami.hanine22@gmail.com>
2021-11-18 09:01:27 +0100

    ajout du contenu de film controllers

--- controllers/film.php
+++ controllers/film.php
@@ -1,3 +1,28 @@
 <?php
-    echo "ok";
+    require_once("bdd.php");
+
+    abstract class Film {
+        function __construct($email, $pwd) {
+            global $bdd;
+
+            $query = $bdd -> prepare('select * from user where email=? and pwd=?');
+            $query -> execute(array($email, $pwd));
+
+            if (isset($querry)) {
+                $this->id = $query['id'];
+                $this->login = $query['login'];
+                $this->pwd = $query['pwd'];
+                $this->email = $query['email'];
+                $this->privilege = $query['privilege'];
+
+                session_start();
+                $_SESSION['user'] = $this;
+                header("Location: pages/home.php");
+            }
+        }
+
+
+
+    }
+
 ?>
